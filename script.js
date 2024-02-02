9fa3ed290cee5efdeef6b0c4f6bc28dd783c3968
Mireya <mmedmer810@g.educaand.es>
2024-02-02 01:49:34 +0100

    V1

---
+++ script.js
@@ -0,0 +1,45 @@
+//script.js
+window.addEventListener("load", inicio, true);
+
+function inicio() {
+    document.getElementById("mensaje").addEventListener("keyup", function () {
+        this.value = this.value.toUpperCase();
+    }, true);
+
+    document.getElementById("cifrar").addEventListener("click", function () {
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("mensaje2").value = cifrar(texto, desplazamiento);
+    }, true);
+
+    document.getElementById("descifrar").addEventListener("click", function () {
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("mensaje2").value = descifrar(texto, desplazamiento);
+    }, true);
+}
+
+function descifrar(texto, desplazamiento) {
+    if (!texto)
+        return '';
+    const letras = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+    desplazamiento = (desplazamiento % 27 - 27) % 27;
+    return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - desplazamiento) % 27]);
+}
+
+function cifrar(texto, desplazamiento) {
+    let resultado = '';
+    let letras = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+    desplazamiento = (desplazamiento % 27 + 27) % 27;
+
+    if (texto) {
+        for (let i = 0; i < texto.length; ++i) {
+            if (letras.indexOf(texto[i]) != -1) {
+                let posicion = ((letras.indexOf(texto[i]) + desplazamiento) % 27);
+                resultado += letras[posicion];
+            } else
+                resultado += texto[i];
+        }
+    }
+    return resultado;
+}
